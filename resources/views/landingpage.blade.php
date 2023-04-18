dbb671fc1a448399002b2c017fe9e2c30e2d7463
Richard Lucas <richardlucasfm@gmail.com>
2023-04-18 19:20:23 +0000

    LP FIX

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,404 @@
+<!DOCTYPE html>
+<!-- This site was created in Webflow. https://www.webflow.com --><!-- Last Published: Tue Apr 11 2023 17:06:26 GMT+0000 (Coordinated Universal Time) -->
+<html
+  data-wf-domain="icm43.webflow.io"
+  data-wf-page="64301e3befaf3a98fd972aa6"
+  data-wf-site="64301e3befaf3a566a972aa5"
+>
+  <head>
+    <meta charset="utf-8" />
+    <title>ICM Sul do Pará - 43 anos</title>
+    <meta
+      content="Convidamos você e sua família para um Culto Especial de glorificação a Deus pelos 43 anos da Igreja Cristã Maranata no estado do Pará."
+      name="description"
+    />
+    <meta content="ICM Sul do Pará - 43 anos" property="og:title" />
+    <meta
+      content="Convidamos você e sua família para um Culto Especial de glorificação a Deus pelos 43 anos da Igreja Cristã Maranata no estado do Pará."
+      property="og:description"
+    />
+    <meta
+      content="https://uploads-ssl.webflow.com/64301e3befaf3a566a972aa5/643023b16cf8eb2c72ba5741_Logo-43-ICM-PA.svg"
+      property="og:image"
+    />
+    <meta content="ICM Sul do Pará - 43 anos" property="twitter:title" />
+    <meta
+      content="Convidamos você e sua família para um Culto Especial de glorificação a Deus pelos 43 anos da Igreja Cristã Maranata no estado do Pará."
+      property="twitter:description"
+    />
+    <meta
+      content="https://uploads-ssl.webflow.com/64301e3befaf3a566a972aa5/643023b16cf8eb2c72ba5741_Logo-43-ICM-PA.svg"
+      property="twitter:image"
+    />
+    <meta property="og:type" content="website" />
+    <meta content="summary_large_image" name="twitter:card" />
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta content="Webflow" name="generator" />
+    <link
+      href="https://uploads-ssl.webflow.com/64301e3befaf3a566a972aa5/css/icm43.webflow.1d6837b83.css"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link href="https://fonts.googleapis.com" rel="preconnect" />
+    <link
+      href="https://fonts.gstatic.com"
+      rel="preconnect"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"
+      type="text/javascript"
+    ></script>
+    <script type="text/javascript">
+      WebFont.load({
+        google: {
+          families: [
+            "Montserrat:100,100italic,200,200italic,300,300italic,400,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic",
+          ],
+        },
+      });
+    </script>
+    <!--[if lt IE 9
+      ]><script
+        src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"
+        type="text/javascript"
+      ></script
+    ><![endif]-->
+    <script type="text/javascript">
+      !(function (o, c) {
+        var n = c.documentElement,
+          t = " w-mod-";
+        (n.className += t + "js"),
+          ("ontouchstart" in o ||
+            (o.DocumentTouch && c instanceof DocumentTouch)) &&
+            (n.className += t + "touch");
+      })(window, document);
+    </script>
+    <link
+      href="https://uploads-ssl.webflow.com/64301e3befaf3a566a972aa5/643590a4c48a581d3df1e9ad_ICO.png"
+      rel="shortcut icon"
+      type="image/x-icon"
+    />
+    <link
+      href="https://uploads-ssl.webflow.com/64301e3befaf3a566a972aa5/643590a751526542f623a705_Webclip.png"
+      rel="apple-touch-icon"
+    />
+  </head>
+  <body>
+    <div
+      data-w-id="90df1c4f-39c4-b447-f6e2-da7fbe846c02"
+      style="opacity: 0"
+      class="navbar-no-shadow wf-section"
+    >
+      <div
+        data-animation="default"
+        data-collapse="medium"
+        data-duration="400"
+        data-easing="ease"
+        data-easing2="ease"
+        role="banner"
+        class="navbar-no-shadow-container w-nav"
+      >
+        <div class="container-regular">
+          <div class="navbar-wrapper">
+            <a
+              href="/"
+              aria-current="page"
+              class="navbar-brand w-nav-brand w--current"
+              ><img
+                src="https://uploads-ssl.webflow.com/64301e3befaf3a566a972aa5/64302634a2fc9f00040bc55a_20220517_224245_0003%20-%20Copia.png"
+                loading="lazy"
+                width="120"
+                alt="Logo da ICM Região Sul do Pará."
+            /></a>
+            <nav role="navigation" class="nav-menu-wrapper w-nav-menu">
+              <ul role="list" class="nav-menu w-list-unstyled">
+                <li class="list-item">
+                  <a href="#" class="nav-link">Sobre o evento</a>
+                </li>
+                <li class="list-item-2">
+                  <a href="#" class="nav-link">Feature</a>
+                </li>
+                <li class="list-item-3">
+                  <a href="#" class="nav-link">User Examples</a>
+                </li>
+                <li class="list-item-4">
+                  <a href="#" class="nav-link">Pricing</a>
+                </li>
+                <li class="mobile-margin-top-10">
+                  <div class="nav-button-wrapper">
+                    <a
+                      href="#sobre-o-evento"
+                      class="button-primary navbar w-button"
+                      >Mais informações</a
+                    >
+                  </div>
+                </li>
+              </ul>
+            </nav>
+            <div class="menu-button w-nav-button">
+              <div class="w-icon-nav-menu"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <section id="Hero" class="hero wf-section">
+      <div class="container">
+        <div class="hero-wrapper">
+          <div class="hero-split">
+            <h1
+              data-w-id="61208035-c31f-1399-b0d1-2fea629cd0c1"
+              style="
+                -webkit-transform: translate3d(0, 52px, 0) scale3d(1, 1, 1)
+                  rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);
+                -moz-transform: translate3d(0, 52px, 0) scale3d(1, 1, 1)
+                  rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);
+                -ms-transform: translate3d(0, 52px, 0) scale3d(1, 1, 1)
+                  rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);
+                transform: translate3d(0, 52px, 0) scale3d(1, 1, 1) rotateX(0)
+                  rotateY(0) rotateZ(0) skew(0, 0);
+                opacity: 0;
+              "
+              class="heading"
+            >
+              Culto especial
+            </h1>
+            <p
+              data-w-id="61208035-c31f-1399-b0d1-2fea629cd0c3"
+              style="
+                opacity: 0;
+                -webkit-transform: translate3d(0px, 60px, 0) scale3d(1, 1, 1)
+                  rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);
+                -moz-transform: translate3d(0px, 60px, 0) scale3d(1, 1, 1)
+                  rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);
+                -ms-transform: translate3d(0px, 60px, 0) scale3d(1, 1, 1)
+                  rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);
+                transform: translate3d(0px, 60px, 0) scale3d(1, 1, 1) rotateX(0)
+                  rotateY(0) rotateZ(0) skew(0, 0);
+              "
+              class="margin-bottom-24px"
+            >
+              Convidamos você e sua família para um Culto Especial de
+              glorificação a Deus pelos 43 anos da Igreja Cristã Maranata no
+              estado do Pará.
+            </p>
+            <a
+              href="#ao-vivo"
+              data-w-id="61208035-c31f-1399-b0d1-2fea629cd0c5"
+              style="opacity: 0"
+              class="button-primary w-button"
+              >Assista aqui</a
+            >
+          </div>
+          <div
+            data-w-id="61208035-c31f-1399-b0d1-2fea629cd0c7"
+            style="opacity: 0"
+            class="hero-split image"
+          >
+            <img
+              src="https://uploads-ssl.webflow.com/64301e3befaf3a566a972aa5/6430253cc1b0ad3a6375f94e_Logo-43-icm-PA-WHITE.svg"
+              loading="lazy"
+              alt="Logo do evento de 43 anos da Igreja Cristã Maranata no estado do Pará."
+              class="shadow-two"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+    <section
+      id="sobre-o-evento"
+      data-w-id="f3308da9-0db0-b62c-7d79-e89ad456c4c3"
+      class="sobre wf-section"
+    >
+      <div class="container">
+        <div class="features-wrapper-two">
+          <div class="features-left">
+            <h2 class="heading-2">Sobre o evento</h2>
+            <p class="features-paragraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse tincidunt sagittis eros. Quisque quis euismod lorem.
+              Etiam sodales ac felis id interdum.
+            </p>
+            <a href="#" class="text-link-arrow w-inline-block"
+              ><div>Learn more</div>
+              <div class="arrow-embed w-embed">
+                <svg
+                  width="17"
+                  height="17"
+                  viewBox="0 0 17 17"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M11.13 8.5L6.87296 4.24291L5.87067 5.24379L9.129 8.5L5.87067 11.7555L6.87225 12.7571L11.13 8.5Z"
+                    fill="currentColor"
+                  />
+                </svg></div
+            ></a>
+          </div>
+          <ul role="list" class="features-right w-list-unstyled">
+            <li class="features-block-two">
+              <a
+                href="https://www.google.com/maps/place/Caraj%C3%A1s+Centro+de+Conven%C3%A7%C3%B5es+-+Leonildo+Borges+Rocha/@-5.3564095,-49.0797408,15z/data=!4m2!3m1!1s0x0:0xb91a4c98d8a99b95?sa=X&amp;ved=2ahUKEwjTiMKCvZ3-AhVQhJUCHYViBYgQ_BJ6BAhZEAg"
+                target="_blank"
+                class="link-block w-inline-block"
+                ><img
+                  src="https://uploads-ssl.webflow.com/64301e3befaf3a566a972aa5/643028a8a523ee8437260fa0_Local-icon.svg"
+                  loading="lazy"
+                  alt="Ícone de localização (demonstrando o local do evento)."
+                  class="features-image"
+                />
+                <p class="item-text">Carajás Centro de Convenções</p></a
+              >
+            </li>
+            <li class="features-block-two">
+              <img
+                src="https://uploads-ssl.webflow.com/64301e3befaf3a566a972aa5/643028a88eaf6457b193d9b4_Data-icon.svg"
+                loading="lazy"
+                alt="Ícone de calendário (demonstrando a data do evento)."
+                class="features-image"
+              />
+              <p class="item-text">28/04 - Sexta-feira</p>
+            </li>
+            <li class="features-block-two">
+              <img
+                src="https://uploads-ssl.webflow.com/64301e3befaf3a566a972aa5/643028a877cdc2271438c9b7_Hora-icon.svg"
+                loading="lazy"
+                alt="Ícone de relógio (demonstrando o horário do evento)."
+                class="features-image"
+              />
+              <p class="item-text">20h00</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section
+      id="ao-vivo"
+      data-w-id="3876c36c-42ae-edd0-9838-3e1ae0a14b45"
+      class="ao-vivo wf-section"
+    >
+      <div class="container">
+        <div class="hero-wrapper-two">
+          <div class="titulo">
+            <img
+              src="https://uploads-ssl.webflow.com/64301e3befaf3a566a972aa5/64302d3d3433e57d2edbea91_live-dot.svg"
+              loading="lazy"
+              alt=""
+              class="image-2"
+            />
+            <h2 class="heading-3">Culto ao vivo</h2>
+          </div>
+          <p class="margin-bottom-24px-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            tincidunt sagittis eros. Quisque quis euismod lorem. Etiam sodales
+            ac felis id interdum.
+          </p>
+          <a href="#" class="button-primary-2 w-button">Get Started</a>
+          <div
+            style="padding-top: 56.17021276595745%"
+            class="w-embed-youtubevideo"
+          >
+            <iframe
+              src="https://www.youtube.com/embed/MJ6jfwZ6mMc?rel=0&amp;controls=1&amp;autoplay=0&amp;mute=0&amp;start=0"
+              frameborder="0"
+              style="
+                position: absolute;
+                left: 0;
+                top: 0;
+                width: 100%;
+                height: 100%;
+                pointer-events: auto;
+              "
+              allow="autoplay; encrypted-media"
+              allowfullscreen=""
+              title="28/04/2023 - [CULTO ESPECIAL 20H] ICM Sul do Pará (Centro de Conveções Carajás) - Sexta-feira"
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="footer-subscribe wf-section">
+      <div class="container">
+        <div class="footer-form-two w-form">
+          <form
+            id="wf-form-Footer-Form-Two"
+            name="wf-form-Footer-Form-Two"
+            data-name="Footer Form Two"
+            method="get"
+            class="footer-form-container-two"
+          >
+            <div class="footer-form-title">
+              Você ou sua família precisa de oração?
+            </div>
+            <p class="paragraph">
+              Ligue para <span class="text-span">0800 707 3076 </span>e seja
+              atendido por um grupo de irmãos prontos para te dar toda a
+              assistência espiritual necessária.
+            </p>
+          </form>
+          <div class="w-form-done">
+            <div>Thank you! Your submission has been received!</div>
+          </div>
+          <div class="w-form-fail">
+            <div>Oops! Something went wrong while submitting the form.</div>
+          </div>
+        </div>
+        <div class="footer-wrapper-three">
+          <div class="footer-block-three">
+            <a
+              href="https://www.igrejacristamaranata.org.br/"
+              target="_blank"
+              class="footer-link-three"
+              >Site da Igreja Cristã Maranata</a
+            ><a
+              href="http://www.radiomaanaim.com.br/"
+              target="_blank"
+              class="footer-link-three"
+              >Rádio Maanaim</a
+            >
+          </div>
+          <div class="footer-social-block-three">
+            <a
+              href="https://www.instagram.com/icm.suldopara/"
+              target="_blank"
+              class="footer-social-link-three w-inline-block"
+              ><img
+                src="https://uploads-ssl.webflow.com/62434fa732124a0fb112aab4/62434fa732124a61f512aaed_instagram%20big.svg"
+                loading="lazy"
+                alt="Ícone do Instagram." /></a
+            ><a
+              href="https://www.youtube.com/@IgrejaCristaMaranataOficial"
+              target="_blank"
+              class="footer-social-link-three w-inline-block"
+              ><img
+                src="https://uploads-ssl.webflow.com/62434fa732124a0fb112aab4/62434fa732124a717f12aaea_youtube%20small.svg"
+                loading="lazy"
+                alt="Ícone do YouTube."
+            /></a>
+          </div>
+        </div>
+        <div class="footer-divider-two"></div>
+        <div class="footer-bottom">
+          <div class="footer-copyright">
+            © 2023 ICM Sul do Pará. Todos os direitos reservados.
+          </div>
+        </div>
+      </div>
+    </section>
+    <script
+      src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=64301e3befaf3a566a972aa5"
+      type="text/javascript"
+      integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://uploads-ssl.webflow.com/64301e3befaf3a566a972aa5/js/webflow.8c8c9552c.js"
+      type="text/javascript"
+    ></script>
+    <!--[if lte IE 9
+      ]><script src="//cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script
+    ><![endif]-->
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+   return view('landingpage');
+});
+
+Route::get('/dashboard', function () {
     return view('index');
 });
 
